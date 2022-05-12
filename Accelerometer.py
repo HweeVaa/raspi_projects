fd46e46deadd640d60679f370b414e1ad2089166
HweeVaa <gnlqkgnlgus@gmail.com>
2022-05-12 18:31:12 +0900

    2nd commit

--- Accelerometer.py
+++ Accelerometer.py
@@ -5,13 +5,21 @@ import adafruit_adxl34x
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import openpyxl as op
 
+#핀 정보 세텡
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 #그래프 데이터 초기화
-t = 0
 AccScala = 0
+
+#엑셀 파일 설정
+wb = op.load_workbook(r"test.xlsx")
+print(wb.sheetnames)
+ws = wb['AccData']
+
+row_num = 1
 
 try:
     while True:
@@ -20,13 +28,14 @@ try:
         y = accelerometer.acceleration[1]
         z = accelerometer.acceleration[2]
 
-        t += 0.01
         AccScala = math.sqrt(x**2 + y**2 + z**2)
 
+        ws.cell(row=row_num, column=1).value = AccScala
         print(AccScala)
 
-        # print(x, y, z)
-        time.sleep(0.01)
+        row_num += 1
+        time.sleep(0.05)
 
-except:
-    KeyboardInterrupt()
+except KeyboardInterrupt:
+    wb.save("result.xlsx")
+    exit(0)
